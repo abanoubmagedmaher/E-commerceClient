927ad25f9e2b288217a4ca8c3fb5550846702100
apple <apple@DESKTOP-76H75II>
2026-01-05 15:02:49 +0200

    first commit

---
+++ src/app/app-module.ts
@@ -0,0 +1,22 @@
+import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing-module';
+import { App } from './app';
+import { NavBar } from './nav-bar/nav-bar';
+
+@NgModule({
+  declarations: [
+    App,
+    NavBar
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideBrowserGlobalErrorListeners()
+  ],
+  bootstrap: [App]
+})
+export class AppModule { }
