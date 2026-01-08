5b1141426de7a5c413a02df5da8f0dc059dfa4b9
apple <apple@DESKTOP-76H75II>
2026-01-08 09:47:59 +0200

    up

--- src/app/shop/shop-module.ts
+++ src/app/shop/shop-module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ShopRoutingModule } from './shop-routing-module';
+import { Shop } from './shop/shop';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    Shop
+  ],
   imports: [
     CommonModule,
     ShopRoutingModule
